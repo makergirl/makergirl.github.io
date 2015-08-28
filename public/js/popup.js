d333e640ef8349e8fe9e97da65c3aa3441a13c1e
Joseph Milla <jjtmilla@gmail.com>
2015-08-27 23:38:33 -0500

    Enable the evil popup :smiling_imp:

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,3 +1,11 @@
 function loadPopup() {
   require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us10.list-manage.com","uuid":"875a6b34dd923b7eda9127124","lid":"4b90876c8d"}) })
 }
+
+jQuery(window).load(function() {
+    document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  });
+
+jQuery('body').on('click', '.mc-closeModal', function() {
+  document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+});
