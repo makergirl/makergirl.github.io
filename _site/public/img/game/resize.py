0c2560c4ee9a4cfe89083548b8dc0a3ca68ce4b1
Joseph Milla <jjtmilla@gmail.com>
2016-04-21 20:58:58 -0500

    Update script

--- _site/public/img/game/resize.py
+++ _site/public/img/game/resize.py
@@ -10,12 +10,27 @@ for root, dirnames, filenames in os.walk(sys.argv[1]):
 print matches
 
 for infile in matches:
-    outfile = os.path.splitext(infile)[0].strip() + ".jpg"
+    outfile = os.path.splitext(infile)[0].strip() + ".png"
     if infile != outfile:
         try:
-            im = Image.open(infile)
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(outfile, "JPEG")
+            # im = Image.open(infile)
+            # im.thumbnail(size, Image.ANTIALIAS)
+            # im.save(outfile, "JPEG")
+
+            img = Image.open(infile)
+            img = img.convert("RGBA")
+            datas = img.getdata()
+
+            newData = []
+            for item in datas:
+                if item[0] == 255 and item[1] == 255 and item[2] == 255:
+                    newData.append((255, 255, 255, 0))
+                else:
+                    newData.append(item)
+
+            img.putdata(newData)
+            img.save(outfile, "JPEG")
+
         except IOError:
             print "Cannot create thumbnail for '%s'" % infile
 
